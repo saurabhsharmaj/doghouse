4afe7b9fdc418f6f542ceae7ed1f758e19629d6a
saurabh <saurabh.find@gmail.com>
2018-05-11 16:53:29 +0530

    add checkboxes

--- DogHouse/src/app/user/user.component.ts
+++ DogHouse/src/app/user/user.component.ts
@@ -1,4 +1,6 @@
-﻿import { OnInit, Component } from "@angular/core";
+﻿import { Group } from './../models/group.model';
+import { Role } from './../models/role.model';
+import { OnInit, Component } from "@angular/core";
 import { User } from '../models/user.model';
 import {CommonService} from '../common.service';
 import {Router} from '@angular/router';
@@ -10,6 +12,8 @@ import {Router} from '@angular/router';
 })
 export class UserComponent implements OnInit {
   private users: Array<User> = [];
+  private roles: Array<Role> = [];
+  private groups: Array<Group> = [];
   private anyErrors:boolean = false;
   private finished:boolean = false;
   p: number = 1;
@@ -19,7 +23,29 @@ export class UserComponent implements OnInit {
               private myRoute: Router) {
   }
 
+  onRoleChange(roleCode, value){
+    console.log(roleCode +" - "+value);
+    let index = this.user.userRoles.indexOf(roleCode);
+    if(value)
+      this.user.userRoles.splice(1,0,roleCode);
+    else
+      this.user.userRoles.splice(index,1);
+  }
+
+  onGroupChange(groupCode,value){
+    console.log(groupCode +" - "+value);
+    
+    let index = this.user.userGroups.indexOf(groupCode);
+    if(value)
+      this.user.userGroups.splice(1,0,groupCode);
+    else
+      this.user.userGroups.splice(index,1);
+      
+  }
+
   ngOnInit() {
+    this.user.userRoles=[];
+    this.user.userGroups=[];
     this.loading=true;
     this.newService.getUsers()
     .subscribe(data =>  {
@@ -29,10 +55,26 @@ export class UserComponent implements OnInit {
     },
     error => this.anyErrors = true,
     () => this.finished = true)
+
+    this.newService.getRoles()
+    .subscribe(data =>  {
+     
+     this.roles=data;
+      
+    })
+
+    this.newService.getGroups()
+    .subscribe(data =>  {
+    
+     this.groups=data;
+    })
+
   }
 
   register(user: User): void{
-    console.log(user);    
+    
+    console.log(user);
+
    
   }
 
